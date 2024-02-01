1e225963cf29a1d8988a1937c57be252ae6c354f
JakeCroz1 <jacobcrosby101@gmail.com>
2024-02-01 11:29:23 -0600

    Reapply "Reapply "Added testimonial, fav icon, meta description, and meta keywords. Corrected spelling error.""
    This reverts commit 1203dd7c650208894025db0040ea9a05d0d6890e.

---
+++ MudBlazorWasmTeleport/wwwroot/js/swipe.js
@@ -0,0 +1,99 @@
+﻿function runSwipe() {
+    /*
+    var swiper = new Swiper(".mySwiper", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "2",
+        loop: true,
+        // spaceBetween: 5,
+        //autoplay: {
+           // delay: 2500,
+           // disableOnInteraction: false,
+        //},
+        //resizeObserver: true,
+        coverflowEffect: {
+            scale: 0.9,
+            rotate: 0,
+            stretch: 0,
+            depth: 0,
+            modifier: 1,
+            slideShadows: true,           
+        },
+        pagination: {
+            el: ".swiper-pagination",
+        },
+    });
+    
+    var swiper = new Swiper(".js-testimonials-slider", {
+        //freeMode: true,
+        grabCursor: true,
+        loop: true,
+        //centeredSlides: true,
+        spaceBetween: 10,
+        slidesPerView: 1,
+        breakpoints: {
+            512: {
+                slidesPerView: 2,
+                spaceBetween: 10,
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 30,
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            },
+        },
+        //autoplay: {
+         //delay: 2500,
+        //disableOnInteraction: false,
+        //},
+        //resizeObserver: true,
+        pagination: {
+            el: ".swiper-pagination",
+            dynamicBullets: true,
+            //clickable: true,
+        }, navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+    */
+    var swiper = new Swiper(".js-testimonials-slider", {
+        //freeMode: true,
+        grabCursor: true,
+        loop: true,
+        spaceBetween: 10,
+        slidesPerView: 1,
+        breakpoints: {
+            512: {
+                slidesPerView: 2,
+                spaceBetween: 44
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 44
+            }, 
+            1024: {
+                slidesPerView: 2,
+                spaceBetween: 44
+            }
+        },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: true
+        },
+        //resizeObserver: true,
+        pagination: {
+            el: ".swiper-pagination",
+            dynamicBullets: true
+            //clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        }
+    });
+}
