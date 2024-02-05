ddcf6095edd3c17a93b5e48f7a7b60275f1a523e
JakeCroz1 <jacobcrosby101@gmail.com>
2024-02-05 14:18:42 -0600

    Reapply "Reapply "Reapply "Added testimonial, fav icon, meta description, and meta keywords. Corrected spelling error."""
    This reverts commit e6daa7da4477098a2a9e9a97cd8f3058615f1b8e.

---
+++ MudBlazorWasmTeleport/wwwroot/js/AnchorNavigation.js
@@ -0,0 +1,17 @@
+﻿function BlazorScrollToId(id) {
+
+    //document.getElementById("mobilemenu").style.setProperty('display', 'none', 'important');
+
+    const element = document.getElementById(id);
+    if (element instanceof HTMLElement) {
+        element.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+            inline: "nearest"
+        });
+    }
+    //document.getElementById("mobilemenu").style.setProperty('display', 'none', 'important');
+    //document.getElementsByClassName("header-mobile-menu").style.setProperty('display', 'none', 'important');
+    //document.getElementById("mobilemenu").style.display = "none !important";
+    // document.getElementsByClassName("header-mobile-menu").style.backgroundColor = "blue";
+}
